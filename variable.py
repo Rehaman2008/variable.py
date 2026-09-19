20c7582c062da2c28f581ab38e5fad66eb02243b
Rehaman2008 <shaikrehamanshareef2008@gmail.com>
2026-09-19 16:54:39 +0530

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,88 @@
+print("Hello World!")
+print("Welcome to Python")
+print("Your Name")
+print("I am learning Python.")
+print("Python Programming")
+print("Good Morning.")
+print("Welcome Students.")
+print("Python is easy.")
+print("My First Program.")
+print("Hello from Python!")
+print("2 + 5")
+print(2 + 5)
+print("10 - 3")
+print(10 - 3)
+print("Hello World!")
+print("Python")
+print("Hello")
+print("Hello")
+print("10 + 20")
+print(10 + 20)
+name = "SHAIK REHAMAN SHAREEF"
+age = 20
+marks = 85
+price = 99.5
+city = "Chennai"
+print(city)
+score = 95
+print(score)
+x = 10
+print(x)
+message = "Welcome"
+print(message)
+a = 10
+b = 20
+print(a + b)
+quantity = 5
+price = 10
+print(quantity*price)
+print(type(25))
+print(type(25.5))
+print(type("25"))
+age = 21
+height = 5.8
+name = "Arun"
+age = 20
+price = 99.5
+name = "Ravi"
+marks = 90
+print(type(marks))
+temperature = 36.5
+print(type(temperature))
+message = "Welcome"
+print(type(message))
+print("Start")
+print("Middle")
+print("End")
+print("First")
+print("Second")
+print("Third")
+x = 10
+print(x)
+x = 20
+print(x)
+number = 5
+print(number)
+number = 10
+print(number)
+print("A")
+print("B")
+print("C")
+name = "Ravi"
+age = 20
+print(name)
+print(age)
+print(1)
+print(2)
+print(3)
+print(4)
+x = 5
+y = 10
+print(x)
+print(y)
+print("Start")
+print(2 + 3)
+print("End")
+print("Login")
+print("Dashboard")
+print("Logout")
